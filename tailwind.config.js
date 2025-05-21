8aa5ed1f968c52d2eeebca346911df546bed6020
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-05-22 00:47:59 +0200

    feat(tailwind.config): add custom color palette for improved theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '24 24 27',
+        background: '24 24 27',
+        foreground: '219 219 219',
+        foregroundMuted: '100 100 100',
+        accent: '77 165 250',
+        accentMuted: '107 165 250',
+      },
+    },
   },
   plugins: [],
 };
