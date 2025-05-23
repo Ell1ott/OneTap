68ca159717270bd1a0f632b7c143ea3c9a473829
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-05-23 16:59:59 +0200

    feat: update app configuration to include Inter font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
     extend: {
       colors: {
         primary: '24 24 27',
-        background: '24 24 27',
-        foreground: '219 219 219',
-        foregroundMuted: '100 100 100',
-        accent: '77 165 250',
+        background: '#FFFFFF',
+        foreground: '#111827',
+        foregroundMuted: '#4b5563',
+        accent: '#42A0F3',
         accentMuted: '107 165 250',
       },
     },
