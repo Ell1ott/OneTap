a82c0efaae0121940860631af1b18a9bf50ea8c9
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-02 20:38:01 +0200

    feat: Make ai be able to add actuals tasks

--- components/tapadoodle/Response.tsx
+++ components/tapadoodle/Response.tsx
@@ -6,10 +6,12 @@ import { View } from 'react-native';
 import { z } from 'zod';
 import { TodoAIData, TodoPreviewCard } from './TodoPreviewCard';
 import { generateAPIUrl } from 'utils/apiUrlHandler';
-import { Todo } from 'components/Todos/classes';
+import { Event, Todo } from 'components/Todos/classes';
 import { PartialDate, Time } from 'components/Todos/types';
 import { parseNaturalDate } from 'utils/dateUtils';
+import { useTasksStore } from 'stores/tasksStore';
 export const Response = ({ transcript }: { transcript: string }) => {
+  const { addTask } = useTasksStore();
   const [responseMessage, setResponseMessage] = useState<string>('');
   const {
     object: _object,
@@ -18,13 +20,24 @@ export const Response = ({ transcript }: { transcript: string }) => {
   } = experimental_useObject({
     api: generateAPIUrl('/api/stream'),
     schema: z.unknown(),
-    onFinish: (object) => {
+    onFinish: ({ object }) => {
       console.log(object);
       console.log(toTodoClass(object as any as TodoAIData));
+      if ((object as any).type === 'todo') {
+        addTask(toTodoClass(object as any as TodoAIData));
+      } else if ((object as any).type === 'event') {
+        const event = toEventClass(object as any as TodoAIData);
+        if (event) {
+          addTask(event);
+        } else {
+          console.log('Error: Event is null');
+        }
+      }
     },
   });
 
   const toTodoClass = (todo: TodoAIData) => {
+    console.log(todo);
     return new Todo({
       id: Date.now().toString() + Math.random().toString(36).substr(2, 9),
       title: todo.title,
@@ -42,6 +55,20 @@ export const Response = ({ transcript }: { transcript: string }) => {
       softRepeat: todo.repeatSoftly && todo.repeat ? new Time(todo.repeat) : undefined,
       amount: todo.amount || undefined,
       category: todo.category || undefined,
+    });
+  };
+
+  const toEventClass = (event: TodoAIData) => {
+    if (!event.start) return null;
+    const start = parseNaturalDate(event.start);
+    if (!start) return null;
+    return new Event({
+      id: Date.now().toString() + Math.random().toString(36).substr(2, 9),
+      title: event.title,
+      emoji: event.emoji,
+      note: event.note,
+      start: start,
+      end: event.end ? parseNaturalDate(event.end) || undefined : undefined,
     });
   };
   const object = _object as any;
