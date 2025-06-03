b91b7ec14c417c0a76ecb9794e7ec01bebab4c00
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-03 13:24:20 +0200

    feat: Add transcripe api for more secure deepgram api handling

---
+++ app/api/transcribe+api.ts
@@ -0,0 +1,36 @@
+import { openai } from '@ai-sdk/openai';
+import { createClient } from '@deepgram/sdk';
+import { streamText } from 'ai';
+
+import { streamObject } from 'ai';
+const client = createClient(process.env.DEEPGRAM_API_KEY);
+export async function GET(req: Request) {
+  console.log('hello');
+  const projectId = await getProjectId();
+  const key = await getTempApiKey(projectId);
+
+  return Response.json({ key: key });
+}
+
+const getProjectId = async () => {
+  const { result, error } = await client.manage.getProjects();
+
+  if (error) {
+    throw error;
+  }
+
+  return result.projects[0].project_id;
+};
+const getTempApiKey = async (projectId: string) => {
+  const { result, error } = await client.manage.createProjectKey(projectId, {
+    comment: 'short lived',
+    scopes: ['usage:write'],
+    time_to_live_in_seconds: 20,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return result;
+};
