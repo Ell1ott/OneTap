df430fa198473c05a61ee55343469ab41f1021c0
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-05 21:36:40 +0200

    Revert "feat: Implement third toaster lib"
    This reverts commit 0a1cf980b2e42d315128382671ce501cc30106d9.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,29 +7,42 @@ import '../utils/polyfills';
 import { useEffect } from 'react';
 import { useTasksStore } from 'stores/tasksStore';
 import { ThemeProvider } from '../components/ThemeProvider';
-import Toast from 'react-native-toast-message';
+import { createNotifications } from 'react-native-notificated';
+const { NotificationsProvider, useNotifications, ...events } = createNotifications();
 export default function RootLayout() {
   const loadTasks = useTasksStore((state) => state.loadTasks);
   useEffect(() => {
     loadTasks();
   }, []);
 
+  const { notify } = useNotifications();
+
+  useEffect(() => {
+    const notificationMetadata = notify('success', {
+      params: {
+        title: 'Hello',
+        description: 'Wow, that was easy',
+      },
+    });
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="system">
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="category"
-          options={{
-            headerShown: false,
-            presentation: 'transparentModal',
-            animation: 'slide_from_right',
-          }}
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-      <Toast />
+      <NotificationsProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="category"
+            options={{
+              headerShown: false,
+              presentation: 'transparentModal',
+              animation: 'slide_from_right',
+            }}
+          />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </NotificationsProvider>
     </ThemeProvider>
   );
 }
