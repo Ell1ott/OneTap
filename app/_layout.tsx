ed122bd5a3d61843988d2e4e4992080d6f10a6bf
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-05 21:35:01 +0200

    feat Improved toasts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useEffect } from 'react';
 import { useTasksStore } from 'stores/tasksStore';
 import { ThemeProvider } from '../components/ThemeProvider';
 import { Toaster } from 'sonner-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   const loadTasks = useTasksStore((state) => state.loadTasks);
@@ -16,21 +17,23 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <ThemeProvider defaultTheme="system">
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="category"
-          options={{
-            headerShown: false,
-            presentation: 'transparentModal',
-            animation: 'slide_from_right',
-          }}
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-      <Toaster position="bottom-center" offset={80} />
-    </ThemeProvider>
+    <GestureHandlerRootView className="">
+      <ThemeProvider defaultTheme="system">
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="category"
+            options={{
+              headerShown: false,
+              presentation: 'transparentModal',
+              animation: 'slide_from_right',
+            }}
+          />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+        <Toaster position="bottom-center" swipeToDismissDirection="left" offset={80} />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
