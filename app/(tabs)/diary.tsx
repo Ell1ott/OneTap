335d7ccb05e8c45e96c9dcdeaa5acea792d4b8d2
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-05 15:49:43 +0200

    chore: Cleanup diary.tsx

--- app/(tabs)/diary.tsx
+++ app/(tabs)/diary.tsx
@@ -1,21 +1,5 @@
-import { Platform, StyleSheet, View } from 'react-native';
-
-import AppText from 'components/base/AppText';
 import { RichTextEditor } from 'components/diary/RichTextEditor';
 
 export default function TabTwoScreen() {
   return <RichTextEditor />;
 }
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
