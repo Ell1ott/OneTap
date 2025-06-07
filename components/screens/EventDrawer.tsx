be8bbae7e092c8bbc68bd7aa373fab5aebaceb11
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-07 19:27:22 +0200

    feat: Make calender open when clicking on date

--- components/screens/EventDrawer.tsx
+++ components/screens/EventDrawer.tsx
@@ -13,16 +13,42 @@ import { TextInput } from 'react-native-gesture-handler';
 
 type TabType = 'Event' | 'Todo';
 
+const formatDate = (date: Date) => {
+  if (date.getFullYear() === new Date().getFullYear()) {
+    return date.toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      weekday: 'short',
+    });
+  }
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
+
+const formatTime = (date: Date) => {
+  return date.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+};
+
 const SelectableText = ({
   children,
   onPress = () => {},
   isSelected = false,
+  pressable = true,
 }: {
   children: React.ReactNode;
   onPress?: () => void;
   isSelected?: boolean;
+  pressable?: boolean;
 }) => (
   <Pressable
+    disabled={!pressable}
     onPress={onPress}
     className={`h-[2rem] flex-1 items-center justify-center rounded-full px-4 ${
       isSelected ? 'bg-card' : 'bg-transparent'
@@ -40,22 +66,6 @@ export default function EventDrawer({ onClose }: { onClose: () => void }) {
   const [title, setTitle] = useState('Volleyball');
   const [date, setDate] = useState(new Date());
   const [activeTab, setActiveTab] = useState<TabType>('Event');
-
-  const formatDate = (date: Date) => {
-    return date.toLocaleDateString('en-US', {
-      weekday: 'short',
-      month: 'short',
-      day: 'numeric',
-    });
-  };
-
-  const formatTime = (date: Date) => {
-    return date.toLocaleTimeString('en-US', {
-      hour: 'numeric',
-      minute: '2-digit',
-      hour12: true,
-    });
-  };
 
   return (
     <Drawer isOpen={true} onClose={onClose} scrollEnabled={false} className="bg-card">
@@ -88,26 +98,7 @@ export default function EventDrawer({ onClose }: { onClose: () => void }) {
             </SelectableText>
           </View>
         </View>
-
-        {/* Date & Time Section */}
-        <View className="mb-6">
-          <View className="mb-4 flex-row items-center">
-            <Icon icon={CalendarIcon} size={20} className="mr-2 text-foregroundMuted" />
-            <AppText className="text-base font-medium text-foregroundMuted">Date & Time</AppText>
-          </View>
-
-          <View className="flex-row space-x-3">
-            {/* Date */}
-            <Pressable className="flex-[1.5] flex-row rounded-full bg-background p-1.5">
-              <SelectableText>{formatDate(date)}</SelectableText>
-            </Pressable>
-
-            {/* Time */}
-            <Pressable className="flex-[1] flex-row rounded-full bg-background p-1.5">
-              <SelectableText>{formatTime(date)}</SelectableText>
-            </Pressable>
-          </View>
-        </View>
+        <DateTime date={date} setDate={setDate} />
 
         {/* Content based on active tab */}
         {activeTab === 'Event' && (
@@ -122,7 +113,49 @@ export default function EventDrawer({ onClose }: { onClose: () => void }) {
   );
 }
 
-export function Calendar({ date, setDate }: { date: DateType; setDate: (date: DateType) => void }) {
+export function DateTime({
+  date,
+  setDate,
+}: {
+  date: DateType;
+  setDate: React.Dispatch<React.SetStateAction<Date>>;
+}) {
+  const [calenderOpen, setCalenderOpen] = useState(false);
+  useEffect(() => {
+    console.log(calenderOpen);
+  }, [calenderOpen]);
+  return (
+    <View className="mb-6">
+      <View className="mb-4 flex-row items-center">
+        <Icon icon={CalendarIcon} size={20} className="mr-2 text-foregroundMuted" />
+        <AppText className="text-base font-medium text-foregroundMuted">Date & Time</AppText>
+      </View>
+
+      <View className="mb-2 flex-row space-x-3">
+        {/* Date */}
+        <Pressable
+          onPress={() => setCalenderOpen(!calenderOpen)}
+          className="flex-[1.5] flex-row rounded-full bg-background p-1.5">
+          <SelectableText pressable={false}>{formatDate(date as Date)}</SelectableText>
+        </Pressable>
+
+        {/* Time */}
+        <Pressable className="flex-[1] flex-row rounded-full bg-background p-1.5">
+          <SelectableText>{formatTime(date as Date)}</SelectableText>
+        </Pressable>
+      </View>
+      {calenderOpen && <Calendar date={date} setDate={setDate} />}
+    </View>
+  );
+}
+
+export function Calendar({
+  date,
+  setDate,
+}: {
+  date: DateType;
+  setDate: React.Dispatch<React.SetStateAction<Date>>;
+}) {
   const defaultClassNames = useDefaultClassNames();
   const [selected, setSelected] = useState<DateType>();
 
@@ -130,7 +163,7 @@ export function Calendar({ date, setDate }: { date: DateType; setDate: (date: Da
     <DateTimePicker
       mode="single"
       date={date}
-      onChange={({ date }) => setDate(date)}
+      onChange={({ date }) => setDate(date as Date)}
       classNames={{
         ...defaultClassNames,
         selected: 'bg-blue-500 border-blue-500',
@@ -145,16 +178,18 @@ export function Calendar({ date, setDate }: { date: DateType; setDate: (date: Da
         today: 'text-blue-500 border-blue-500 border-[3px]',
         today_label: 'text-blue-500',
       }}
-      styles={{
-        day: {
-          // flex: 'none',
-          height: 32,
-        },
-        day_cell: {
-          flex: 'none',
-          height: 32,
-        },
-      }}
+      containerHeight={250}
+      // styles={{
+      //   day: {
+      //     flex: 'none',
+      //     aspectRatio: 1,
+      //   },
+      //   day_cell: {
+      //     flex: 'none',
+      //     aspectRatio: 1,
+      //   },
+
+      // }}
     />
   );
 }
