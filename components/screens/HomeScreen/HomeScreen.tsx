7ab5de8a0f36fc25f47cbb2c8d392937234e4603
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-08 03:06:15 +0200

    fix expo crypto package and make legandApp todos work

--- components/screens/HomeScreen/HomeScreen.tsx
+++ components/screens/HomeScreen/HomeScreen.tsx
@@ -19,29 +19,29 @@ import { todos$ as _todos$ } from 'utils/SupaLegend';
 import { observer } from '@legendapp/state/react';
 import { FlatList } from 'react-native';
 
-// const Todos = observer(({ todos$ }: { todos$: typeof _todos$ }) => {
-//   // Get the todos from the state and subscribe to updates
-//   const todos = todos$.get();
-//   const renderItem = ({ item: todo }: { item: Tables<'todos'> }) => (
-//     <View>
-//       <AppText>{todo.title}</AppText>
-//     </View>
-//   );
-//   if (todos) return <FlatList data={Object.values(todos)} renderItem={renderItem} />;
-//   return <></>;
-// });
+const Todos = observer(({ todos$ }: { todos$: typeof _todos$ }) => {
+  // Get the todos from the state and subscribe to updates
+  const todos = todos$.get();
+  const renderItem = ({ item: todo }: { item: Tables<'todos'> }) => (
+    <View>
+      <AppText>{todo.title}</AppText>
+    </View>
+  );
+  if (todos) return <FlatList data={Object.values(todos)} renderItem={renderItem} />;
+  return <></>;
+});
 
 export function HomeScreen() {
   const { tasks } = useTasksStore();
   const [openCategory, setOpenCategory] = useState<string | null>(null);
   const [openAddEvent, setOpenAddEvent] = useState(false);
-  console.log(_todos$.get());
   return (
     <>
       {openCategory && (
         <CategoryDrawer category={openCategory} onClose={() => setOpenCategory(null)} />
       )}
       {/* <Todos todos$={_todos$} /> */}
+      <Todos todos$={_todos$} />
       <ScrollView
         className="flex-1 bg-background"
         contentContainerClassName="px-6 pt-16 pb-6"
