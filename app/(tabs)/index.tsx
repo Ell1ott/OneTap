4676d98e65665c21cfd27783ffc3435dada20fde
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-09 23:02:05 +0200

    feat: Cleanup unused code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,6 @@ import { Event } from 'components/Todos/classes';
 import { Todo } from 'components/Todos/classes/Todo';
 import { events$, todos$ } from 'utils/supabase/SupaLegend';
 
-export const tasks$ = observable(() => {
-  const todos = todos$.get();
-  const events = events$.get();
-  return [
-    ...Object.values(todos).map((t) => new Todo(t)),
-    ...Object.values(events).map((e) => new Event(e)),
-  ] as (Todo | Event)[];
-});
 export default function HomeScreen() {
-  return <HomeScreenComponent tasks$={tasks$} />;
+  return <HomeScreenComponent />;
 }
