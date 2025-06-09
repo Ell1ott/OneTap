a35dad962adbb018cf03d24a0d0a0c80ccfa035a
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-09 21:46:26 +0200

    Migrate response ai insertion

--- components/tapadoodle/Response.tsx
+++ components/tapadoodle/Response.tsx
@@ -11,6 +11,7 @@ import { useTasksStore } from 'stores/tasksStore';
 import { fetch as expoFetch } from 'expo/fetch';
 import { supabaseAnonAuthHeaders } from 'utils/supabase/supabaseAuth';
 import { TablesInsert } from 'utils/supabase/database.types';
+import { addTodo } from 'utils/supabase/SupaLegend';
 export const Response = ({ transcript }: { transcript: string }) => {
   const { addTask } = useTasksStore();
   const [responseMessage, setResponseMessage] = useState<string>('');
@@ -29,7 +30,12 @@ export const Response = ({ transcript }: { transcript: string }) => {
       console.log(toTodoClass(object as any as TodoAIData));
       if (!(object as any).title) return;
       if ((object as any).type === 'todo') {
-        addTask(toTodoClass(object as any as TodoAIData));
+        const todo = toTodoClass(object as any as TodoAIData);
+        if (todo) {
+          addTodo(todo);
+        } else {
+          console.log('Error: Todo is null');
+        }
       } else if ((object as any).type === 'event') {
         const event = toEventClass(object as any as TodoAIData);
         if (event) {
@@ -44,18 +50,23 @@ export const Response = ({ transcript }: { transcript: string }) => {
     },
   });
 
-
   const toTodoClass = (todo: TodoAIData) => {
     console.log(todo);
 
     const start = todo.start
-      ? HumanDate.fromNaturalString(Array.isArray(todo.start) ? todo.start[0] : todo.start)?.toDictionary()
+      ? HumanDate.fromNaturalString(
+          Array.isArray(todo.start) ? todo.start[0] : todo.start
+        )?.toDictionary()
       : undefined;
     const end = todo.end
-      ? HumanDate.fromNaturalString(Array.isArray(todo.end) ? todo.end[0] : todo.end)?.toDictionary()
+      ? HumanDate.fromNaturalString(
+          Array.isArray(todo.end) ? todo.end[0] : todo.end
+        )?.toDictionary()
       : undefined;
     const remind_at = todo.remindAt
-      ? HumanDate.fromNaturalString(Array.isArray(todo.remindAt) ? todo.remindAt[0] : todo.remindAt)?.toDictionary()
+      ? HumanDate.fromNaturalString(
+          Array.isArray(todo.remindAt) ? todo.remindAt[0] : todo.remindAt
+        )?.toDictionary()
       : undefined;
 
     const r: TablesInsert<'todos'> = {
@@ -64,32 +75,17 @@ export const Response = ({ transcript }: { transcript: string }) => {
       note: todo.note,
       start: start,
       end: end,
-      soft_due: todo.softDue ? HumanDate.fromNaturalString(todo.softDue)?.toDictionary() : undefined,
+      soft_due: todo.softDue
+        ? HumanDate.fromNaturalString(todo.softDue)?.toDictionary()
+        : undefined,
       remind_at: remind_at ? [remind_at] : undefined,
-      repeat: todo.repeat
-      soft_repeat: todo.repeatSoftly
-      amount: todo.amount || undefined,
-      category: todo.category || undefined,
-    }
+      repeat: todo.repeat,
+      soft_repeat: todo.repeatSoftly ? todo.repeat : undefined,
+      category: todo.category,
+      completed: Array(todo.amount || 1).fill(false),
+    };
 
-    return new Todo({
-      id: Date.now().toString() + Math.random().toString(36).substr(2, 9),
-      title: todo.title,
-      emoji: todo.emoji,
-      note: todo.note,
-      start: todo.start
-        ? HumanDate.fromNaturalString(Array.isArray(todo.start) ? todo.start[0] : todo.start)
-        : undefined,
-      end: todo.end
-        ? HumanDate.fromNaturalString(Array.isArray(todo.end) ? todo.end[0] : todo.end)
-        : undefined,
-      softDue: todo.softDue ? HumanDate.fromNaturalString(todo.softDue) : undefined,
-      remindAt: todo.remindAt ? HumanDate.fromNaturalString(todo.remindAt) : undefined,
-      repeat: todo.repeat && !todo.repeatSoftly ? new Time(todo.repeat) : undefined,
-      softRepeat: todo.repeatSoftly && todo.repeat ? new Time(todo.repeat) : undefined,
-      amount: todo.amount || undefined,
-      category: todo.category || undefined,
-    });
+    return r;
   };
 
   const toHumanDateArray = (date: string | string[] | null): HumanDate[] | undefined => {
