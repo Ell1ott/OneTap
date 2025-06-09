fcedfc3ad3cbfe86f7213d7076a454a63dde45e5
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-09 21:44:29 +0200

    Revert "try to fix randomBytes error"
    This reverts commit d59fcf08d7bff7ec99ab279d7ad4f0d7f0765606.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import { observable } from '@legendapp/state';
-import AppText from 'components/base/AppText';
 import { HomeScreen as HomeScreenComponent } from 'components/screens/HomeScreen/HomeScreen';
-import { Event, TaskCategory } from 'components/Todos/classes';
+import { Event } from 'components/Todos/classes';
 import { Todo } from 'components/Todos/classes/Todo';
-import { categories$, events$, todos$ } from 'utils/supabase/SupaLegend';
+import { events$, todos$ } from 'utils/supabase/SupaLegend';
 
+export const tasks$ = observable(() => {
+  const todos = todos$.get();
+  const events = events$.get();
+  return [
+    ...Object.values(todos).map((t) => new Todo(t)),
+    ...Object.values(events).map((e) => new Event(e)),
+  ] as (Todo | Event)[];
+});
 export default function HomeScreen() {
-  return <HomeScreenComponent />;
+  return <HomeScreenComponent tasks$={tasks$} />;
 }
--- utils/supabase/SupaLegend.ts
+++ utils/supabase/SupaLegend.ts
@@ -4,8 +4,10 @@ import { configureSynced, synced } from '@legendapp/state/sync';
 import { syncedSupabase } from '@legendapp/state/sync-plugins/supabase';
 import { observablePersistAsyncStorage } from '@legendapp/state/persist-plugins/async-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v4 as uuidv4 } from 'uuid';
 import { Database, TablesInsert } from './database.types';
 import { Event, TaskCategory, Todo } from 'components/Todos/classes';
+
 const supabase = createClient<Database>(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
@@ -23,9 +25,7 @@ export async function signInAnonymously() {
   }
 }
 
-export const generateId = () => {
-  return Date.now().toString();
-};
+export const generateId = () => uuidv4();
 // Create a configured sync function
 const customSynced = configureSynced(syncedSupabase, {
   // Use React Native Async Storage
