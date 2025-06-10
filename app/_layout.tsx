31b85957d5232f42dd86ba2e78d126d166822d2b
Elliott <85990359+Ell1ott@users.noreply.github.com>
2025-06-11 01:34:19 +0200

    fix: random values dep not working without log statement

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,16 @@ import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import '../global.css';
 import '../utils/polyfills';
+import 'react-native-get-random-values';
 import { useEffect } from 'react';
 import { ThemeProvider } from '../components/ThemeProvider';
 import { Toaster } from 'sonner-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../assets/font.css';
 import { signInAnonymously } from 'utils/supabase/SupaLegend';
+import { v4 as uuidv4 } from 'uuid';
 export default function RootLayout() {
-
+  console.log(uuidv4());
   useEffect(() => {
     signInAnonymously().then((data) => {
       console.log(data);
