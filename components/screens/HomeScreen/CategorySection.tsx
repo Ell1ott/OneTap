25e40009963bd17fb56962409a696b4d99d241c4
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-06-21 10:36:27 -0700

    make categories scrollable if too many

--- components/screens/HomeScreen/CategorySection.tsx
+++ components/screens/HomeScreen/CategorySection.tsx
@@ -1,6 +1,6 @@
 import AppText from 'components/base/AppText';
 import { observer } from '@legendapp/state/react';
-import { Pressable, View } from 'react-native';
+import { Pressable, ScrollView, View } from 'react-native';
 import { categories$, todos$ } from 'utils/supabase/SupaLegend';
 import { TaskCategory } from 'components/Todos/classes';
 import { Tables } from 'utils/supabase/database.types';
@@ -9,23 +9,28 @@ import { router } from 'expo-router';
 export const CategorySection = observer(() => {
   const categories = Object.values(categories$.get(true));
   console.log('rerendering category section');
+  const Wrapper = categories.length > 2 ? ScrollView : View;
   return (
-    <View className="mb-6 flex-row gap-2">
+    <Wrapper
+      className="-mx-4 mb-6 flex-none"
+      contentContainerClassName="gap-2 px-4"
+      showsHorizontalScrollIndicator={false}
+      horizontal={categories.length > 2}>
       {categories.map((category: Tables<'categories'>, index: number) => (
         <CategoryCard key={category.id} id={category.id} index={index} />
       ))}
-    </View>
+    </Wrapper>
   );
 });
 
 export const CategoryCard = observer(({ id, index }: { id: string; index: number }) => {
-  const category = categories$.get(true)[id];
+  const category = categories$[id].get();
   const todos = Object.values(todos$.get(true));
   const categoryTodos = todos.filter((todo) => todo.category === category.id);
   const undoneTodos = categoryTodos.filter((todo) => todo.completed?.includes(false));
 
   const colors = [
-    'bg-blue-100',
+    'bg-blue-100 dark:bg-blue-900/10',
     'bg-[#D5F5E8]',
     'bg-red-100',
     'bg-yellow-100',
@@ -48,7 +53,9 @@ export const CategoryCard = observer(({ id, index }: { id: string; index: number
       key={category.id}
       className={`flex-1 flex-row gap-1 rounded-xl ${randomColor} p-4`}>
       <AppText className="text-lg font-medium text-foreground">{undoneTodos.length}</AppText>
-      <AppText className="text-lg font-medium text-foreground/50">{category.title}</AppText>
+      <AppText numberOfLines={1} className="text-lg font-medium text-foreground/50">
+        {category.title}
+      </AppText>
     </Pressable>
   );
 });
